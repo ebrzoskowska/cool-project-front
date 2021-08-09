ebc0b6254327c2ae991a7f82c62a835e52564019
Emilia Brzoskowska <emilia.brzoskowska@gmail.com>
2021-08-09 08:09:58 +0100

    file structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Too cool to be true.</h1>
+    </div>
+  );
+}
+
+export default App;
