e7064af691af4925a0528be28a011cd43210cad4
kbalev <kamen_balev@yahoo.com>
2021-08-11 16:33:30 +0100

    threw together a basic navbar and routing to the account page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import './index.css'
-
 import { LandingPage } from './pages/landing';
 import { Shop } from './pages/shop';
+import {Account} from './pages/accountDetails'
+import { Navbar } from './navbar';
 
 const App = () => {
 
@@ -11,12 +12,16 @@ const App = () => {
 
   return (
     <Router>
+      <Navbar/>
       {user ? <Redirect to='/shop'/> : <Redirect to='/'/>}
       <Route exact path='/'>
         <LandingPage setUser={setUser}/>
       </Route>
       <Route path='/shop'>
         <Shop/>
+      </Route>
+      <Route path='/accountDetails'>
+        <Account setUser={setUser} user={user}/>
       </Route>
     </Router>
   );
---
+++ src/pages/accountDetails/index.js
@@ -0,0 +1,10 @@
+import {React} from 'react';
+
+
+export const Account = ({setUser, user}) =>{
+    return(
+        <div>
+            <h1>This is where the account page goes.</h1>
+        </div>
+    )
+}
